7625bf94a73f076b6fd34c505825484c33df0448
Vithushan <sutharshanvithushan@gmail.com>
2026-01-27 07:11:29 +0530

    make directories more prominent, and don't select line numbers

--- template/repo-branch-tree.go
+++ template/repo-branch-tree.go
@@ -123,7 +123,7 @@ func (p RepoBranchTreePage) Body() (body string) {
 			rowTemplate := template.Must(template.New("row").Parse(`<tr><td class="isbinary" id="{{.Type}}">{{.Type}}</td><td><a href="{{.URLRoot}}/branch/{{.Branch}}/tree/{{.FilePath}}/{{.Entry.Name}}/">{{.Entry.Name}}</a></td><td></td><td></td><td></td><td class="filemode">{{.Entry.Mode.ToOSFileMode}}</td></tr>`))
 			if entry.Mode == filemode.Submodule {
 				fileType = "sub"
-				rowTemplate = template.Must(template.New("row").Parse(`<tr><td class="isbinary" id="{{.Type}}">{{.Type}}</td><td>{{.Entry.Name}}</td><td></td><td></td><td></td><td class="filemode">{{.Entry.Mode.ToOSFileMode}}</td></tr>`))
+				rowTemplate = template.Must(template.New("row").Parse(`<tr><td class="isbinary" id="{{.Type}}">{{.Type}}</td><td class="tree-dir">{{.Entry.Name}}</td><td></td><td></td><td></td><td class="filemode">{{.Entry.Mode.ToOSFileMode}}</td></tr>`))
 			}
 			rowTemplate.Execute(&rowBuffer, Row{&p.Config.URLRoot, &p.Branch, &p.FilePath, &entry, nil, "", nil, fileType})
 		}
