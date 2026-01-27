fbe6a9b14e3c00f85e8d00cbd201d2ac8548c1b4
Vithushan <sutharshanvithushan@gmail.com>
2026-01-28 00:48:35 +0530

    uncomment config

--- config/config.go
+++ config/config.go
@@ -5,29 +5,29 @@ const SizePrecision = 2
 
 // IRRELEVANT FOR MULTI USER SETUP
 
-// var IndexPageConfig PageConfig = PageConfig{
-// 	URLRoot: "/",
-// 	RootDir: "/home/acutewoof/gitbrowse",
-// 	Title: "Gitbrowse on git.lewoof.xyz",
-// 	Description: "lewoof on git.lewoof.xyz",
-// 	Thumbnail: "/static/thumbnail.png",
-// 	Favicon: "/static/favicon.ico",
-// 	Styles: []string{
-// 		"/static/global.css",
-// 	},
-// }
-//
-// // <REPO> will be replaced by the name of the repository
-// // this is the config used for all the tabs on the repo page
-// var RepoPageConfig PageConfig = PageConfig{
-// 	URLRoot: "/browse/<REPO>",
-// 	CloneURL: "https://git.lewoof.xyz/clone/<REPO>",
-// 	RootDir: "/home/acutewoof/gitbrowse/<REPO>",
-// 	Title: "lewoof/<REPO>",
-// 	Description: "<REPO> on git.lewoof.xyz",
-// 	Thumbnail: "/static/thumbnail.png",
-// 	Favicon: "/static/favicon.ico",
-// 	Styles: []string{
-// 		"/static/global.css",
-// 	},
-// }
+var IndexPageConfig PageConfig = PageConfig{
+	URLRoot: "/",
+	RootDir: "/home/acutewoof/gitbrowse",
+	Title: "Gitbrowse on git.lewoof.xyz",
+	Description: "lewoof on git.lewoof.xyz",
+	Thumbnail: "/static/thumbnail.png",
+	Favicon: "/static/favicon.ico",
+	Styles: []string{
+		"/static/global.css",
+	},
+}
+
+// <REPO> will be replaced by the name of the repository
+// this is the config used for all the tabs on the repo page
+var RepoPageConfig PageConfig = PageConfig{
+	URLRoot: "/browse/<REPO>",
+	CloneURL: "https://git.lewoof.xyz/clone/<REPO>",
+	RootDir: "/home/acutewoof/gitbrowse/<REPO>",
+	Title: "lewoof/<REPO>",
+	Description: "<REPO> on git.lewoof.xyz",
+	Thumbnail: "/static/thumbnail.png",
+	Favicon: "/static/favicon.ico",
+	Styles: []string{
+		"/static/global.css",
+	},
+}
